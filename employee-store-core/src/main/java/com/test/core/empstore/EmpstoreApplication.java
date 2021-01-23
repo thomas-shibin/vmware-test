11cee6370fc1ecca09226f223133f1d49cba5c54
Shibin <shibin.thomas@oracle.com>
2021-01-23 13:28:59 +0530

    commit

---
+++ employee-store-core/src/main/java/com/test/core/empstore/EmpstoreApplication.java
@@ -0,0 +1,13 @@
+package com.test.core.empstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpstoreApplication.class, args);
+	}
+
+}
